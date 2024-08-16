c8c5f16301dcbac356355eda9b354ccebe6a621a
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-08-16 15:47:08 -0400

    write test to assert fail with in proper Permission authority

--- tests/03_initLpVault.ts
+++ tests/03_initLpVault.ts
@@ -1,15 +1,19 @@
 import * as anchor from "@coral-xyz/anchor";
 import { assert } from "chai";
-import { superAdminProgram, tokenMintA } from "./rootHooks";
+import { superAdminProgram, tokenMintA, tokenMintB } from "./rootHooks";
 import { getAssociatedTokenAddressSync } from "@solana/spl-token";
+import { WasabiSolana } from "../target/types/wasabi_solana";
 
 describe("InitLpVault", () => {
+  const program = anchor.workspace.WasabiSolana as anchor.Program<WasabiSolana>;
+
+  const [superAdminPermissionKey] =
+    anchor.web3.PublicKey.findProgramAddressSync(
+      [anchor.utils.bytes.utf8.encode("super_admin")],
+      program.programId,
+    );
+
   it("should create the LP Vault", async () => {
-    const [superAdminPermissionKey] =
-      anchor.web3.PublicKey.findProgramAddressSync(
-        [anchor.utils.bytes.utf8.encode("super_admin")],
-        superAdminProgram.programId,
-      );
     await superAdminProgram.methods
       .initLpVault()
       .accounts({
@@ -33,8 +37,34 @@ describe("InitLpVault", () => {
     // Validate the LpVault state was set
     assert.equal(lpVaultAfter.totalAssets.toNumber(), 0);
     assert.equal(lpVaultAfter.asset.toString(), tokenMintA.toString());
-    const vaultAddress = getAssociatedTokenAddressSync(tokenMintA, lpVaultKey, true);
+    const vaultAddress = getAssociatedTokenAddressSync(
+      tokenMintA,
+      lpVaultKey,
+      true,
+    );
     assert.equal(lpVaultAfter.vault.toString(), vaultAddress.toString());
     assert.equal(lpVaultAfter.sharesMint.toString(), sharesMint.toString());
+  });
+
+  describe("non permissioned signer", () => {
+    it("should fail", async () => {
+      try {
+        await program.methods
+          .initLpVault()
+          .accounts({
+            payer: program.provider.publicKey,
+            permission: superAdminPermissionKey,
+            assetMint: tokenMintB,
+          })
+          .rpc();
+        assert.ok(false);
+      } catch (err) {
+        if (err instanceof anchor.AnchorError) {
+          assert.equal(err.error.errorCode.number, 2001);
+        } else {
+          assert.ok(false);
+        }
+      }
+    });
   });
 });
