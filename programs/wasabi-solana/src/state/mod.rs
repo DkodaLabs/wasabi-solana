c96fe853e69ad7ca2cafbda8d99dd825ca2e1a5a
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-08-20 13:28:31 -0400

    add base_pool state

--- programs/wasabi-solana/src/state/mod.rs
+++ programs/wasabi-solana/src/state/mod.rs
@@ -1,7 +1,10 @@
 pub mod global_settings;
 pub mod lp_vault;
 pub mod permission;
+pub mod base_pool;
 
 pub use global_settings::*;
 pub use lp_vault::*;
 pub use permission::*;
+pub use base_pool::*;
+
