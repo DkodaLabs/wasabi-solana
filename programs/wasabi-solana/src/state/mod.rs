8203e42c0377e3398b82d465062a922b46fc9f71
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-08-21 13:03:40 -0400

    add Position struct

--- programs/wasabi-solana/src/state/mod.rs
+++ programs/wasabi-solana/src/state/mod.rs
@@ -1,10 +1,11 @@
+pub mod base_pool;
 pub mod global_settings;
 pub mod lp_vault;
 pub mod permission;
-pub mod base_pool;
+pub mod position;
 
+pub use base_pool::*;
 pub use global_settings::*;
 pub use lp_vault::*;
 pub use permission::*;
-pub use base_pool::*;
-
+pub use position::*;
