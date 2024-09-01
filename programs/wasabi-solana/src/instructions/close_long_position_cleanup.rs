f66b3771f42f395dad14abff3eb929646210e286
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-09-01 16:13:06 -0400

    test passing for double setup

--- programs/wasabi-solana/src/instructions/close_long_position_cleanup.rs
+++ programs/wasabi-solana/src/instructions/close_long_position_cleanup.rs
@@ -1,10 +1,18 @@
 use anchor_lang::prelude::*;
 
+use crate::utils::get_function_hash;
+
 #[derive(Accounts)]
 pub struct CloseLongPositionCleanup<'info> {
   owner: Signer<'info>,
   
 
+}
+
+impl<'info> CloseLongPositionCleanup<'info> {
+  pub fn get_hash() -> [u8; 8] {
+    get_function_hash("global", "close_long_position_cleanup")
+}
 }
 
 pub fn handler(_ctx: Context<CloseLongPositionCleanup>) -> Result<()> {
--- programs/wasabi-solana/src/instructions/close_long_position_setup.rs
+++ programs/wasabi-solana/src/instructions/close_long_position_setup.rs
@@ -1,7 +1,9 @@
 use anchor_lang::{prelude::*, solana_program::sysvar};
 use anchor_spl::token::TokenAccount;
 
-use crate::{BasePool, ClosePositionRequest, Permission, Position};
+use crate::{utils::position_setup_transaction_introspecation_validation, BasePool, ClosePositionRequest, Permission, Position};
+
+use super::CloseLongPositionCleanup;
 
 #[derive(Accounts)]
 pub struct CloseLongPositionSetup<'info> {
@@ -49,31 +51,22 @@ pub struct CloseLongPositionSetup<'info> {
 
 impl<'info> CloseLongPositionSetup<'info> {
   pub fn validate(ctx: &Context<Self>, args: &CloseLongPositionArgs) -> Result<()> {
-      // let now = Clock::get()?.unix_timestamp;
-
-      
-
-      // if now > args.expiration {
-      //     return Err(ErrorCode::PositionReqExpired.into());
-      // }
-
-      // if !ctx.accounts.permission.can_cosign_swaps() {
-      //     return Err(ErrorCode::InvalidSwapCosigner.into());
-      // }
-      Ok(())
+    // Validate TX only has only one setup IX and has one following cleanup IX
+    position_setup_transaction_introspecation_validation(&ctx.accounts.sysvar_info, CloseLongPositionCleanup::get_hash())?;
+    Ok(())
   }
+}
 
 #[derive(AnchorDeserialize, AnchorSerialize)]
 pub struct CloseLongPositionArgs {
-    /// The minimum amount out required when swapping
-    pub min_target_amount: u64,
-    /// The timestamp when this close position request expires.
-    pub expiration: i64,
+  /// The minimum amount out required when swapping
+  pub min_target_amount: u64,
+  /// The timestamp when this close position request expires.
+  pub expiration: i64,
 }
 
 
 pub fn handler(_ctx: Context<CloseLongPositionSetup>, _args: CloseLongPositionArgs) -> Result<()> {
-  // TODO: Transaction introspection
   // TODO: Pull the collateral from the LongPool vault
   // TODO: Create a close position request
 
