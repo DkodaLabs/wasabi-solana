28aaf8bfb9e71733da0e4a95e29cab5df118dd3a
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-09-10 13:45:41 -0400

    add all event types

--- programs/wasabi-solana/src/events.rs
+++ programs/wasabi-solana/src/events.rs
@@ -34,3 +34,62 @@ impl NewVault {
         }
     }
 }
+
+#[event]
+pub struct PositionOpened {
+    pub position: Pubkey,
+    pub trader: Pubkey,
+    pub currency: Pubkey,
+    pub collateral_currency: Pubkey,
+    pub down_payment: u64,
+    pub principal: u64,
+    pub collateral_amount: u64,
+    pub fees_to_be_paid: u64,
+}
+
+#[event]
+pub struct PositionClosed {
+    pub position: Pubkey,
+    pub trader: Pubkey,
+    pub payout: u64,
+    pub principal_repaid: u64,
+    pub interest_paid: u64,
+    pub fee_amount: u64
+}
+
+#[event]
+pub struct PositionClosedWithOrder {
+    pub position: Pubkey,
+    pub trader: Pubkey,
+    pub order_type: u8,
+    pub payout: u64,
+    pub principal_repaid: u64,
+    pub interest_paid: u64,
+    pub fee_amount: u64
+}
+
+#[event]
+pub struct PositionLiquidated{
+    pub position: Pubkey,
+    pub trader: Pubkey,
+    pub payout: u64,
+    pub principal_repaid: u64,
+    pub interest_paid: u64,
+    pub fee_amount: u64,
+}
+
+#[event]
+pub struct PositionClaimed {
+    pub position: Pubkey,
+    pub trader: Pubkey,
+    pub amount_claimed: u64,
+    pub principal_repaid: u64,
+    pub interest_paid: u64,
+    pub fee_amount: u64
+}
+
+pub struct NativeYieldClaimed { 
+    pub vault: Pubkey,
+    pub token: Pubkey,
+    pub amount: u64
+}
