da9d274d7ce5c9cddda2d609f38ac4e91ae3458f
Tommy Johnson <tommyjohnson1028@gmail.com>
2024-09-13 14:55:35 -0400

    open position created in test

--- tests/14_claimPosition.ts
+++ tests/14_claimPosition.ts
@@ -33,8 +33,17 @@ describe("ClaimPosition", () => {
     program.provider.publicKey,
     false,
   );
-  const [lpVaultKey] = anchor.web3.PublicKey.findProgramAddressSync(
+  const ownerTokenB = getAssociatedTokenAddressSync(
+    tokenMintB,
+    program.provider.publicKey,
+    false,
+  );
+  const [lpVaultKeyA] = anchor.web3.PublicKey.findProgramAddressSync(
     [anchor.utils.bytes.utf8.encode("lp_vault"), tokenMintA.toBuffer()],
+    program.programId,
+  );
+  const [lpVaultKeyB] = anchor.web3.PublicKey.findProgramAddressSync(
+    [anchor.utils.bytes.utf8.encode("lp_vault"), tokenMintB.toBuffer()],
     program.programId,
   );
   const [longPoolBKey] = anchor.web3.PublicKey.findProgramAddressSync(
@@ -55,19 +64,116 @@ describe("ClaimPosition", () => {
     longPoolBKey,
     true,
   );
+  const [shortPoolAKey] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      anchor.utils.bytes.utf8.encode("short_pool"),
+      tokenMintA.toBuffer(),
+      tokenMintB.toBuffer(),
+    ],
+    program.programId,
+  );
+  const shortPoolAVaultKey = getAssociatedTokenAddressSync(
+    tokenMintA,
+    shortPoolAKey,
+    true,
+  );
+  const shortPoolACurrencyVaultKey = getAssociatedTokenAddressSync(
+    tokenMintB,
+    shortPoolAKey,
+    true,
+  );
 
   before(async () => {});
 
   describe("Short position", async () => {
+    let positionKey: anchor.web3.PublicKey;
     before(async () => {
-      // TODO: Create a short position
+      const nonce = 0;
+      const fee = new anchor.BN(10);
+      [positionKey] = anchor.web3.PublicKey.findProgramAddressSync(
+        [
+          anchor.utils.bytes.utf8.encode("position"),
+          program.provider.publicKey.toBuffer(),
+          shortPoolAKey.toBuffer(),
+          lpVaultKeyB.toBuffer(),
+          new anchor.BN(nonce).toArrayLike(Buffer, "le", 2),
+        ],
+        program.programId,
+      );
+      const now = new Date().getTime() / 1_000;
+      const downPayment = new anchor.BN(1_000);
+      // amount to be borrowed
+      const principal = new anchor.BN(1_000);
+      const swapAmount = principal;
+      const minTargetAmount = new anchor.BN(1);
+      const setupIx = await program.methods
+        .openShortPositionSetup({
+          nonce: 0,
+          minTargetAmount,
+          downPayment,
+          principal,
+          currency: tokenMintA,
+          expiration: new anchor.BN(now + 3_600),
+          fee,
+        })
+        .accounts({
+          owner: program.provider.publicKey,
+          ownerCurrencyAccount: ownerTokenB,
+          ownerTargetCurrencyAccount: ownerTokenA,
+          lpVault: lpVaultKeyB,
+          shortPool: shortPoolAKey,
+          permission: coSignerPermission,
+          authority: SWAP_AUTHORITY.publicKey,
+          feeWallet: feeWalletA,
+          globalSettings: globalSettingsKey,
+        })
+        .instruction();
+      const [swapAuthority] = anchor.web3.PublicKey.findProgramAddressSync(
+        [abSwapKey.publicKey.toBuffer()],
+        TOKEN_SWAP_PROGRAM_ID,
+      );
+      const swapIx = TokenSwap.swapInstruction(
+        abSwapKey.publicKey,
+        swapAuthority,
+        SWAP_AUTHORITY.publicKey,
+        shortPoolACurrencyVaultKey,
+        swapTokenAccountB,
+        swapTokenAccountA,
+        shortPoolAVaultKey,
+        poolMint,
+        poolFeeAccount,
+        null,
+        tokenMintB,
+        tokenMintA,
+        TOKEN_SWAP_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        TOKEN_PROGRAM_ID,
+        BigInt(swapAmount.toString()),
+        BigInt(minTargetAmount.toString()),
+      );
+      await program.methods
+        .openShortPositionCleanup()
+        .accounts({
+          owner: program.provider.publicKey,
+          shortPool: shortPoolAKey,
+          position: positionKey,
+          lpVault: lpVaultKeyB,
+        })
+        .preInstructions([setupIx, swapIx])
+        .signers([SWAP_AUTHORITY])
+        .rpc({ skipPreflight: true });
     });
     it("should successfully pay loan and return collateral to user", async () => {
+      const positionBefore = await program.account.position.fetch(positionKey);
+
+      assert.ok(positionBefore.trader.equals(program.provider.publicKey));
       // TODO: validate Position is closed
       // TODO: validate principal and interest was paid by the trader
       // TODO: validate the LP Vault received the interest and principal
       // TODO: Validate the Trader recevied the collateral.
       // TODO: Validate the Pool's collateral_vault paid the collateral.
+      assert.ok(false);
     });
   });
 
@@ -87,7 +193,7 @@ describe("ClaimPosition", () => {
           anchor.utils.bytes.utf8.encode("position"),
           program.provider.publicKey.toBuffer(),
           longPoolBKey.toBuffer(),
-          lpVaultKey.toBuffer(),
+          lpVaultKeyA.toBuffer(),
           new anchor.BN(nonce).toArrayLike(Buffer, "le", 2),
         ],
         program.programId,
@@ -105,9 +211,9 @@ describe("ClaimPosition", () => {
         .accounts({
           owner: program.provider.publicKey,
           ownerCurrencyAccount: ownerTokenA,
-          // @ts-ignore 
+          // @ts-ignore
           currencyVault: longPoolBCurrencyVaultKey,
-          lpVault: lpVaultKey,
+          lpVault: lpVaultKeyA,
           longPool: longPoolBKey,
           permission: coSignerPermission,
           authority: SWAP_AUTHORITY.publicKey,
