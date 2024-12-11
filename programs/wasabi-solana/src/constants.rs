74f47e10f29c7e590ba8e8ed689fa941c95c280b
serfrae <andrew@dkoda.xyz>
2024-12-11 15:59:11 +0100

    renamed functions to be more descriptive and agnostic across staking protocols

--- programs/wasabi-solana/src/constants.rs
+++ programs/wasabi-solana/src/constants.rs
@@ -2,6 +2,29 @@ use anchor_lang::prelude::*;
 #[constant]
 pub const SEED: &str = "anchor";
 
+#[derive(Eq, PartialEq)]
+pub enum StakeProvider {
+    Jito = 0,
+    Marinade = 1
+}
+
+//impl StakeProvider {
+//    pub fn from_u8(value: u8) -> Result<Self> {
+//        match value {
+//            0 => Ok(StakeProvider::Jito),
+//            1 => Ok(StakeProvider::Marinade),
+//            _ => Err(error!(ErrorCode::InvalidStakeProvider)),
+//        }
+//    }
+//
+//    pub fn to_u8(&self) -> u8 {
+//        match self {
+//            StakeProvider::Jito => 0,
+//            StakeProvider::Marinade => 1,
+//        }
+//    }
+//}
+//
 pub const JITO_STAKE_POOL: &str = "Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb";
 pub const JITO_WITHDRAW_AUTHORITY: &str = "6iQKfEyhr3bZMotVkW6beNZz5CPAkiwvgV2CTje9pVSS";
 pub const JITO_RESERVE_STAKE_ACCOUNT: &str = "BgKUXdS29YcHCFrPm5M8oLHiTzZaMDjsebggjoaQ6KFL";
