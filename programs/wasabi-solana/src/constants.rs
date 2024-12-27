bc61884e05727e1e2b67739da6944b2fa8ed2300
Andrew Fraser <andrew@dkoda.xyz>
2024-12-27 17:49:41 +0000

    JITO

--- programs/wasabi-solana/src/constants.rs
+++ programs/wasabi-solana/src/constants.rs
@@ -1,32 +1,19 @@
 use anchor_lang::prelude::*;
+
 #[constant]
 pub const SEED: &str = "anchor";
 
-#[derive(Eq, PartialEq)]
-pub enum StakeProvider {
-    Jito = 0,
-    Marinade = 1
-}
-
-//impl StakeProvider {
-//    pub fn from_u8(value: u8) -> Result<Self> {
-//        match value {
-//            0 => Ok(StakeProvider::Jito),
-//            1 => Ok(StakeProvider::Marinade),
-//            _ => Err(error!(ErrorCode::InvalidStakeProvider)),
-//        }
-//    }
-//
-//    pub fn to_u8(&self) -> u8 {
-//        match self {
-//            StakeProvider::Jito => 0,
-//            StakeProvider::Marinade => 1,
-//        }
-//    }
-//}
-//
+#[constant]
 pub const JITO_STAKE_POOL: &str = "Jito4APyf642JPZPx3hGc6WWJ8zPKtRbRs4P815Awbb";
+
+#[constant]
 pub const JITO_WITHDRAW_AUTHORITY: &str = "6iQKfEyhr3bZMotVkW6beNZz5CPAkiwvgV2CTje9pVSS";
+
+#[constant]
 pub const JITO_RESERVE_STAKE_ACCOUNT: &str = "BgKUXdS29YcHCFrPm5M8oLHiTzZaMDjsebggjoaQ6KFL";
+
+#[constant]
 pub const JITO_FEE_ACCOUNT: &str = "feeeFLLsam6xZJFc6UQFrHqkvVt4jfmVvi2BRLkUZ4i";
+
+#[constant]
 pub const JITO_POOL_TOKEN_MINT: &str = "J1toso1uCk3RLmjorhTtrVwY9HJ7X8V9yYac6Y7kGCPn";
